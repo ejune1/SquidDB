ee6f20055957809f02c90eff944d0c040a9aef54
Eric June <ejune1@asu.edu>
2026-07-03 14:39:34 -0400

    fix build

--- include/storage/Schema.h
+++ include/storage/Schema.h
@@ -1,10 +1,18 @@
 #ifndef SCHEMA_H
 #define SCHEMA_H
 
-namespace sqiddb { namespace storage {
+#include "storage/Column.h"
+
+#include <vector>
+
+namespace squiddb { namespace storage {
 
 class Schema {
 
+
+	private:
+		std::vector<Column> m_column;
+		std::vector<size_t> m_offset;
 };
 
 }} // namespace
